3a99f1bea3bbdea8194e3b2cf950e2b01c99d70a
Jhhames <fjhhames@gmail.com>
2018-12-18 10:52:09 +0100

    remove lorem

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,10 +9,13 @@
             <div class="card-body">
                 <div class="row">
                     <div class="col-md-6">
-                        As Employee
-                        <p>
+                        <h1>
+                            Employee?? <span class="fa fa-user"></span>
+
+                        </h1>
+                        {{-- <p>
                             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quam eveniet aliquid praesentium perspiciatis vero, voluptate non quis quas officiis repellat dolorem possimus? Officiis nostrum blanditiis sed voluptatem dolor a, impedit quod commodi eveniet non magni nobis eum ullam facere esse soluta similique explicabo minus quo possimus quas placeat alias?
-                        </p>
+                        </p> --}}
                         <a href="/employee/login">
                             <button class="btn btn-primary ">
                                 Click here to Sign in
@@ -20,10 +23,10 @@
                         </a>
                     </div>
                     <div class="col-md-6">
-                        As Admin
-                        <p>
+                        <h1>Admin?? </h1>
+                        {{-- <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia laboriosam facere hic possimus minus magni numquam autem, nulla alias ipsa dolore beatae, in aspernatur culpa architecto mollitia enim sed sint magnam officiis eligendi est? Qui ipsa delectus molestias aspernatur veniam ea temporibus optio esse quo, quibusdam, cupiditate fuga quae nisi?
-                        </p>
+                        </p> --}}
                         <a href="/login">
                             <button class="btn btn-success">
                                 Click here to Sign in
